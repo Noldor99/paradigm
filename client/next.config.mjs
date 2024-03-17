a11ff6b29870462e9fa5991019bfd2ad133f7197
Noldor99 <qwerr19942017@gmail.com>
2024-03-17 11:27:52 +0200

    simple

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: 'localhost' },
-      { hostname: 'https://paradigm-qbzl.onrender.com/' },
+      { hostname: 'paradigm-qbzl.onrender.com' },
       { hostname: 'loremflickr.com' },
     ],
   },
