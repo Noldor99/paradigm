9c914406a303aeae8d76ce502ba18095490b9b1b
Noldor99 <qwerr19942017@gmail.com>
2024-03-17 11:56:59 +0200

    ss

--- client/src/lib/urlRevalidate.ts
+++ client/src/lib/urlRevalidate.ts
@@ -1,5 +1,5 @@
 
-const url = process.env.NEXT_PUBLIC_REVALIDATE_URL || 'http://localhost:3000/api'
+const url = process.env.NEXT_PUBLIC_REVALIDATE_URL || 'https://paradigm-sand.vercel.app/api'
 const secret = process.env.NEXT_PUBLIC_TOKEN_REVALIDATE || 'simple'
 
 export const urlRevalidate = (router: string) => {
